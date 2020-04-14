9aaa480c0dec8d9b01ec2d0e87882432567c6c76
DMG <dmwaigithinji@gmail.com>
2020-04-14 14:32:43 +0300

    Implement keyboard functions for block selection

--- src/components/grid/index.tsx
+++ src/components/grid/index.tsx
@@ -1,17 +1,70 @@
 import React, { FC, useEffect, Dispatch, useCallback } from 'react';
+import useMousetrap from 'react-hook-mousetrap';
 import { Container, Row } from './styles';
 import Block from './block';
-import { useDispatch } from 'react-redux';
-import { createGrid } from 'reducers';
+import { useDispatch, useSelector } from 'react-redux';
+import { createGrid, IReducer, selectBlock } from 'reducers';
 import { AnyAction } from 'redux';
-import { INDEX } from 'typings';
+import { INDEX, BLOCK_COORDS } from 'typings';
+
+interface IState {
+  selectedBlock?: BLOCK_COORDS;
+}
 
 const Grid: FC = () => {
+  const state = useSelector<IReducer, IState>(({ selectedBlock }) => ({
+    selectedBlock,
+  }));
   const dispatch = useDispatch<Dispatch<AnyAction>>();
   const create = useCallback(() => dispatch(createGrid()), [dispatch]);
   useEffect(() => {
     create();
   }, [create]);
+
+  function moveDown() {
+    if (state.selectedBlock && state.selectedBlock[0] < 8)
+      dispatch(
+        selectBlock([
+          (state.selectedBlock[0] + 1) as INDEX,
+          state.selectedBlock[1],
+        ])
+      );
+  }
+
+  function moveLeft() {
+    if (state.selectedBlock && state.selectedBlock[1] > 0)
+      dispatch(
+        selectBlock([
+          state.selectedBlock[0],
+          (state.selectedBlock[1] - 1) as INDEX,
+        ])
+      );
+  }
+
+  function moveRight() {
+    if (state.selectedBlock && state.selectedBlock[1] < 8)
+      dispatch(
+        selectBlock([
+          state.selectedBlock[0],
+          (state.selectedBlock[1] + 1) as INDEX,
+        ])
+      );
+  }
+
+  function moveUp() {
+    if (state.selectedBlock && state.selectedBlock[0] > 0)
+      dispatch(
+        selectBlock([
+          (state.selectedBlock[0] - 1) as INDEX,
+          state.selectedBlock[1],
+        ])
+      );
+  }
+
+  useMousetrap('down', moveDown);
+  useMousetrap('left', moveLeft);
+  useMousetrap('right', moveRight);
+  useMousetrap('up', moveUp);
 
   return (
     <Container data-cy="grid-container">
@@ -20,9 +73,10 @@ const Grid: FC = () => {
           <Row data-cy="grid-row-container">
             {React.Children.toArray(
               [...Array(9)].map((_, colIndex) => (
-                <Block 
-                  rowIndex={rowIndex as INDEX} 
-                  colIndex={colIndex as INDEX} />
+                <Block
+                  rowIndex={rowIndex as INDEX}
+                  colIndex={colIndex as INDEX}
+                />
               ))
             )}
           </Row>
