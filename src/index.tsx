03e5d2fb0823795d4875e5484186266064f73bd0
DMG <dmwaigithinji@gmail.com>
2020-04-16 09:58:15 +0300

    Register service worker

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-import { unregister, configureStore } from "core";
+import { register, configureStore } from "core";
 import { App } from "components";
 
 const { persistor, store } = configureStore();
@@ -21,4 +21,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-unregister();
+register();
